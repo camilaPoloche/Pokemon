da1ba3e9064ddc28034eb5a538247f6f22cd3ed4
marianaRivas02 <rivasclavijom12@gmail.com>
2025-03-31 16:51:23 -0500

    crecion interface pokemon electrico

---
+++ src/autonoma/pokemon/models/PokemonElectrico.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.pokemon.models;
+
+/**
+ *
+ * @author maria
+ */
+public interface PokemonElectrico {
+    
+    public abstract String atacarImpacTrueno();
+    public abstract String atacarPunioTrueno();
+    public abstract String atacarRayo();
+    public abstract String atacarRayoCarga();
+    
+}
