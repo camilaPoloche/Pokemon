c185158580f083174bd2aee598ae830cf37a23f9
marianaRivas02 <rivasclavijom12@gmail.com>
2025-03-31 17:22:52 -0500

    creacion pokemon final

--- src/autonoma/pokemon/models/Pikachu.java
+++ src/autonoma/pokemon/models/Pikachu.java
@@ -8,6 +8,53 @@ package autonoma.pokemon.models;
  *
  * @author maria
  */
-public class Pikachu {
+public class Pikachu extends Pokemon implements PokemonElectrico {
+
+    public Pikachu(String numPokedex, String nombre, float peso, String temporada) {
+        super(numPokedex, nombre, peso, temporada);
+    }
+
+    
+    @Override
+    public String atacarPlacaje() {
+         return "soy pikachu y estoy atacando con placaje";
+    }
+
+    @Override
+    public String atacarArañazo() {
+          return "soy pikachu y estoy atacando con arañazo";
+    }
+
+    @Override
+    public String atacarMordisco() {
+          return "soy pikachu y estoy atacando con mordisco";
+    }
+
+    @Override
+    public String atacarImpacTrueno() {
+         return "soy pikachu y estoy atacando con immpacto de trueno";
+    }
+
+    @Override
+    public String atacarPunioTrueno() {
+         return "soy pikachu y estoy atacando con puño de tueno";
+    }
+
+    @Override
+    public String atacarRayo() {
+         return "soy pikachu y estoy atacando con rayo";
+    }
+
+    @Override
+    public String atacarRayoCarga() {
+         return "soy pikachu y estoy atacando con rayo de carga";
+    }
+    
+    
+
+  
+    
+    
+    
     
 }
